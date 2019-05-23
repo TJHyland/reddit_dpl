ce126ec30bc3199a252d91e62446cacdf8705bab
Tyson Hyland <tyhyland@hotmail.com>
2019-05-22 20:59:01 -0600

    Added subs, topics, and partially added comments.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,30 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root route
+    # controller#action 
+  root 'subs#index'
+
+  # resources
+    # resources :controller
+    resources :subs do
+      resources :topics
+    end
+
+    resources :topics do
+      resources :comments
+    end
+    
+    # only: [:index, :show]
+    # can also do except: [:routes]
+    # generates all necessary routes/prefixes
+    # http://localhost:3000/rails/info/routes
+
+  # custom routes
+    # http verb 'controller/action', as: bananas
+    # get '/edit', to: 'subs#edit'
+    # get '/show', to: 'subs#show'
+    # get '/subs/:id' to: 'subs#destroy'
+
+
+    # /subs/1/topics/1
+    # /subs/:sub_id/topics/:id
 end
